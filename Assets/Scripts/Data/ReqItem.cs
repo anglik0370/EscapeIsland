0664bf28888d9183a1bbbd4d42d281211d07d1f2
Anglik <seonwoo6429@gmail.com>
2022-01-11 18:31:07 +0900

    ReqItem 클래스 작성

--- Assets/Scripts/Data/ReqItem.cs
+++ Assets/Scripts/Data/ReqItem.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReqItem : MonoBehaviour
+[System.Serializable]
+public class ReqItem
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public ItemSO item;
+    public int needItemAmount;
 }
