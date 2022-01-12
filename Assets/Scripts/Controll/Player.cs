cfade29b641fbdd67985f69c78af3e3b88d70bec
hwanjuson <hwanjuson@gmail.com>
2022-01-12 19:03:39 +0900

    urp로 교체, 플레이어 이동

--- Assets/Scripts/Controll/Player.cs
+++ Assets/Scripts/Controll/Player.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigid;
+
+    public int id;
+    public bool isRemote;
+    public Inventory inventory;
+    public Color color;
+    public Vector2 targetPos;
+
+    public Vector2 moveDir = new Vector2();
+
+    float xMove { get; set; }
+    float yMove { get; set; }
+
+    [SerializeField]
+    private int speed = 5;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+    public void InitPlayer()
+    {
+
+    }
+
+    public void Move()
+    {
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.y = Input.GetAxis("Vertical");
+
+        transform.Translate(moveDir * speed * Time.deltaTime);
+    }
+
+    IEnumerator SendPosition()
+    {
+        yield return null;
     }
 }
