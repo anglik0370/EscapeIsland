5e949ebec73b04eb45a6ffd036d11644add98272
Anglik <seonwoo6429@gmail.com>
2022-01-23 18:58:51 +0900

    ProgressUI 스크립트 추가 작성

--- Client/Assets/Scripts/UI/ProgressUI.cs
+++ Client/Assets/Scripts/UI/ProgressUI.cs
@@ -6,14 +6,19 @@ using UnityEngine.UI;
 public class ProgressUI : MonoBehaviour
 {
     private Text percentText;
+    private Image gaugeImg;
+
+    private const float MAX_AMOUNT = 100f;
 
     private void Awake() 
     {
         percentText = GameObject.Find("Text").GetComponent<Text>();
+        gaugeImg = GameObject.Find("Gauge").GetComponent<Image>();
     }
 
     public void UpdateProgress(float progress)
     {
         percentText.text = $"{Mathf.RoundToInt(progress)}%";
+        gaugeImg.fillAmount = progress / MAX_AMOUNT;
     }
 }
