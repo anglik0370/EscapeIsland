701defef0df822ec03843c783c8468e63e6bf4bb
hwanjuson <hwanjuson@gmail.com>
2022-03-22 15:33:01 +0900

    SocketClinet.cs handler 추가 방식 변경

--- Client/Assets/Scripts/Network/SocketClient.cs
+++ Client/Assets/Scripts/Network/SocketClient.cs
@@ -31,6 +31,20 @@ public class SocketClient : MonoBehaviour
         handlerDic = new Dictionary<string, IMsgHandler>();
     }
 
+    
+
+    private void Start()
+    {
+        IMsgHandler[] handlerList = handlerParent.GetComponents<IMsgHandler>();
+        for (int i = 0; i < handlerList.Length; i++)
+        {
+            //print(GetTypeString(handlerList[i].GetType().ToString()));
+            handlerDic.Add(GetTypeString(handlerList[i].GetType().ToString()), handlerList[i]);
+        }
+
+        //webSocket = new WebSocket($"{url}:{port}");
+        ConnectSocket("localhost", port.ToString());
+    }
     public string GetTypeString(string s)
     {
         List<int> idx = new List<int>();
@@ -38,7 +52,7 @@ public class SocketClient : MonoBehaviour
 
         for (int i = 1; i < s.Length; i++)
         {
-            if(s[i].Equals(char.ToUpper(s[i])))
+            if (s[i].Equals(char.ToUpper(s[i])))
             {
                 idx.Add(i);
             }
@@ -65,59 +79,6 @@ public class SocketClient : MonoBehaviour
 
 
         return returnStr.ToUpper();
-    }
-
-    private void Start()
-    {
-        //핸들러 딕셔너리에 필요한것들 추가해주기
-        string handlerPath = Application.dataPath + "/Scripts/Network/Handler/";
-        //IMsgHandler[] test = Resources.LoadAll<>("/Handler");
-        IMsgHandler[] handlerList = handlerParent.GetComponents<IMsgHandler>();
-        for (int i = 0; i < handlerList.Length; i++)
-        {
-            print(GetTypeString(handlerList[i].GetType().ToString()));
-        }
-
-        
-
-        //DirectoryInfo di = new DirectoryInfo(handlerPath);
-
-        //FileInfo[] fileInfo = di.GetFiles();
-
-        //for (int i = 0; i < fileInfo.Length; i++)
-        //{
-        //    print(fileInfo[i].);
-        //}
-        
-
-        handlerDic.Add("LOGIN", handlerParent.GetComponent<LoginHandler>());
-        handlerDic.Add("ENTER_ROOM", handlerParent.GetComponent<EnterRoomHandler>());
-        handlerDic.Add("REFRESH_ROOM", handlerParent.GetComponent<RefreshRoomHandler>());
-        handlerDic.Add("REFRESH_USER", handlerParent.GetComponent<RefreshUserHandler>());
-        handlerDic.Add("REFRESH_MASTER", handlerParent.GetComponent<RefreshMasterHandler>());
-        handlerDic.Add("GAME_START", handlerParent.GetComponent<GameStartHandler>());
-        handlerDic.Add("EXIT_ROOM", handlerParent.GetComponent<ExitRoomHandler>());
-        handlerDic.Add("ERROR", handlerParent.GetComponent<ErrorHandler>());
-        handlerDic.Add("DISCONNECT", handlerParent.GetComponent<DisconnectHandler>());
-        handlerDic.Add("GET_ITEM", handlerParent.GetComponent<GetItemHandler>());
-        handlerDic.Add("STORAGE_DROP", handlerParent.GetComponent<StorageDropHandler>());
-        handlerDic.Add("START_REFINERY", handlerParent.GetComponent<StartConvertingHandler>());
-        handlerDic.Add("RESET_REFINERY", handlerParent.GetComponent<ResetConverterHandler>());
-        handlerDic.Add("TAKE_REFINERY", handlerParent.GetComponent<TakeConverterHandler>());
-        handlerDic.Add("TIME_REFRESH", handlerParent.GetComponent<TimeRefreshHandler>());
-        handlerDic.Add("KILL", handlerParent.GetComponent<KillHandler>());
-        handlerDic.Add("VOTE_TIME", handlerParent.GetComponent<VoteTimeHandler>());
-        handlerDic.Add("CHAT", handlerParent.GetComponent<ChatHandler>());
-        handlerDic.Add("VOTE_COMPLETE", handlerParent.GetComponent<VoteCompleteHandler>());
-        handlerDic.Add("VOTE_DIE", handlerParent.GetComponent<VoteDieHandler>());
-        handlerDic.Add("VOTE_TIME_END", handlerParent.GetComponent<VoteTimeEndHandler>());
-        handlerDic.Add("STORAGE_FULL", handlerParent.GetComponent<StorageFullHandler>());
-        handlerDic.Add("WIN_KIDNAPPER", handlerParent.GetComponent<WinKidnapperHandler>());
-        handlerDic.Add("WIN_CITIZEN", handlerParent.GetComponent<WinCitizenHandler>());
-        handlerDic.Add("TIMER", handlerParent.GetComponent<TimerHandler>());
-
-        //webSocket = new WebSocket($"{url}:{port}");
-        ConnectSocket("localhost", port.ToString());
     }
 
     public void ConnectSocket(string ip, string port)
@@ -153,7 +114,7 @@ public class SocketClient : MonoBehaviour
             DataVO vo = packetList.Dequeue();
             if (handlerDic.TryGetValue(vo.type, out handler))
             {
-                //print(vo.type);
+                print(vo.type);
                 handler.HandleMsg(vo.payload);
             }
             else
