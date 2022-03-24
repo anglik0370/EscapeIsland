6b6b569e5c3efda596f00194d783b038a4705ca6
hwanjuson <hwanjuson@gmail.com>
2022-03-24 16:51:51 +0900

    오류 수정

--- Client/Assets/Scripts/Manager/Server/NetworkManager.cs
+++ Client/Assets/Scripts/Manager/Server/NetworkManager.cs
@@ -171,32 +171,6 @@ public class NetworkManager : MonoBehaviour
         SendManager.Instance.SendCharacterChange(so.id, beforeId);
     }
 
-    public void SetStartConvert(int converterId, int itemSOId)
-    {
-        ItemSO so = ItemManager.Instance.FindItemSO(itemSOId);
-
-        Debug.Log($"변환기{converterId}에서 {so}변환 시작");
-
-        GameManager.Instance.refineryList.Find(x => x.id == converterId).ConvertingStart(so);
-
-        print("start");
-    }
-
-    public void SetResetConverter(int converterId)
-    {
-        ItemConverter converter = GameManager.Instance.refineryList.Find(x => x.id == converterId);
-        converter.ConvertingReset();
-        print("reset");
-    }
-
-    public void SetTakeConverterAfterItem(int converterId)
-    {
-        ItemConverter converter = GameManager.Instance.refineryList.Find(x => x.id == converterId);
-        converter.TakeIAfterItem();
-        //refinery.ingotItem = null;
-        print("take");
-    }
-
     public void EnterRoom()
     {
         PopupManager.instance.ClosePopup();
--- Client/Assets/Scripts/Network/InGame/Converter.cs
+++ Client/Assets/Scripts/Network/InGame/Converter.cs
@@ -10,21 +10,21 @@ public class Converter : ISetAble
 
         Debug.Log($"변환기{converterId}에서 {so}변환 시작");
 
-        GameManager.Instance.refineryList.Find(x => x.id == converterId).ConvertingStart(so);
+        ConverterManager.Instance.ConverterList.Find(x => x.id == converterId).ConvertingStart(so);
 
         print("start");
     }
 
     public void SetResetConverter(int converterId)
     {
-        ItemConverter converter = GameManager.Instance.refineryList.Find(x => x.id == converterId);
+        ItemConverter converter = ConverterManager.Instance.ConverterList.Find(x => x.id == converterId);
         converter.ConvertingReset();
         print("reset");
     }
 
     public void SetTakeConverterAfterItem(int converterId)
     {
-        ItemConverter converter = GameManager.Instance.refineryList.Find(x => x.id == converterId);
+        ItemConverter converter = ConverterManager.Instance.ConverterList.Find(x => x.id == converterId);
         converter.TakeIAfterItem();
         //refinery.ingotItem = null;
         print("take");
