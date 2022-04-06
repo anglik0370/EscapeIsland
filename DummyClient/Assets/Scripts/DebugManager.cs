b8933d5f140fe7689ca82d72684616756b3eef4a
hwanjuson <hwanjuson@gmail.com>
2022-04-06 20:13:44 +0900

    대충 틀 다 만들고 현재 login,create room, join(find) room, kill 되는거 확인

--- DummyClient/Assets/Scripts/DebugManager.cs
+++ DummyClient/Assets/Scripts/DebugManager.cs
@@ -1,16 +1,139 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum SendType
 {
-    CREATE_ROOM = 0,
+    LOGIN = 0,
+    CREATE_ROOM,
     JOIN_ROOM,
     KILL,
-    LOGIN,
 }
 
 public class DebugManager : MonoBehaviour
 {
-    
+    public static DebugManager Instance { get; private set; }
+
+    private SendType curSendType = SendType.LOGIN;
+    public SendType CurSendType => curSendType;
+
+    private Dictionary<SendType, CanvasGroup> panelDic;
+
+    public Transform cgParent;
+    private CanvasGroup[] cgArr;
+    [SerializeField]
+    private Dropdown sendTypeDropdown;
+    [SerializeField]
+    private Button sendBtn;
+
+    [Header("로그인 관련 UI들")]
+    public InputField nameInputField;
+
+    [Header("룸 생성 관련 UI들")]
+    public InputField roomNameInputField;
+    public InputField kidnapperCountInputField;
+    public InputField maxPlayerInputField;
+
+    [Header("룸 조인 관련 UI들")]
+    public InputField joinRoomNameInputField;
+
+    [Header("킬 관련 UI들")]
+    public InputField targetSocketIdInputField;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        panelDic = new Dictionary<SendType, CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        sendTypeDropdown.onValueChanged.AddListener(x =>
+        {
+            curSendType = (SendType)x;
+            ChangeSendType(CurSendType);
+        });
+
+        cgArr = cgParent.GetComponentsInChildren<CanvasGroup>();
+
+        for (int i = 0; i < cgArr.Length; i++)
+        {
+            panelDic.Add((SendType)i, cgArr[i]);
+        }
+
+        curSendType = SendType.LOGIN;
+        ChangeSendType(curSendType);
+    }
+
+    private void ChangeSendType(SendType type)
+    {
+        foreach (SendType st in panelDic.Keys)
+        {
+            CanvasGroupOnOff(panelDic[st], st == type);
+        }
+        SetSendBtn(type);
+    }
+
+    private void CanvasGroupOnOff(CanvasGroup cg, bool on)
+    {
+        cg.alpha = on ? 1f : 0f;
+        cg.interactable = on;
+        cg.blocksRaycasts = on;
+    }
+
+    private void SetSendBtn(SendType type)
+    {
+        sendBtn.onClick.RemoveAllListeners();
+        sendBtn.onClick.AddListener(() => SendMessage(type.ToString()));
+    }
+
+    private void LOGIN()
+    {
+        LoginVO vo = new LoginVO(nameInputField.text);
+
+        DataVO dataVO = new DataVO("LOGIN", JsonUtility.ToJson(vo));
+
+        SocketClient.SendDataToSocket(JsonUtility.ToJson(dataVO));
+
+        nameInputField.text = "";
+    }
+
+    private void CREATE_ROOM()
+    {
+        RoomVO vo = new RoomVO(roomNameInputField.text, 0, 0, int.Parse(maxPlayerInputField.text), int.Parse(kidnapperCountInputField.text));
+
+        DataVO dataVO = new DataVO("CREATE_ROOM", JsonUtility.ToJson(vo));
+
+        SocketClient.SendDataToSocket(JsonUtility.ToJson(dataVO));
+
+        roomNameInputField.text = "";
+        maxPlayerInputField.text = "";
+        kidnapperCountInputField.text = "";
+    }
+
+    private void JOIN_ROOM()
+    {
+        RoomVO vo = new RoomVO();
+        vo.name = joinRoomNameInputField.text;
+
+        DataVO dataVO = new DataVO("FIND_ROOM", JsonUtility.ToJson(vo));
+
+        SocketClient.SendDataToSocket(JsonUtility.ToJson(dataVO));
+
+        joinRoomNameInputField.text = "";
+    }
+
+    private void KILL()
+    {
+        KillVO vo = new KillVO(int.Parse(targetSocketIdInputField.text));
+
+        DataVO dataVO = new DataVO("KILL", JsonUtility.ToJson(vo));
+
+        SocketClient.SendDataToSocket(JsonUtility.ToJson(dataVO));
+
+        targetSocketIdInputField.text = "";
+    }
 }
