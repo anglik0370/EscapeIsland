7dbe737f8407338527dbeb8f8d5deb00bf587756
Anglik <seonwoo6429@gmail.com>
2022-04-12 09:53:55 +0900

    미션 타입 Enum 선언, 미션 패널 스크립트 생성 및 작성중

--- Client/Assets/Scripts/UI/Panel/MissionPanel.cs
+++ Client/Assets/Scripts/UI/Panel/MissionPanel.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class MissionPanel : MonoBehaviour
+public enum MissionType
 {
-    // Start is called before the first frame update
-    void Start()
+    Wood,
+    Coconut,
+    Berry,
+    Ore,
+    Water,
+    Battery,
+    Engine,
+}
+
+public class MissionPanel : Panel
+{
+    [SerializeField]
+    private List<IMission> missionList = new List<IMission>();
+
+    [SerializeField]
+    private Transform missionParentTrm;
+
+    [SerializeField]
+    private IMission oldMission;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+
+        missionList = missionParentTrm.GetComponentsInChildren<IMission>().ToList();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Open(MissionType type)
     {
-        
+
+    }
+
+    private void SetMission(IMission mission)
+    {
+        if(oldMission == null)
+        {
+            oldMission = mission;
+        }
+
+        if(oldMission.MissionType != mission.MissionType)
+        {
+            for (int i = 0; i < missionList.Count; i++)
+            {
+                missionList[i].Cvs.alpha = 0;
+                missionList[i].Cvs.blocksRaycasts = false;
+                missionList[i].Cvs.interactable = false;
+            }
+        }
+
+        mission.Cvs.alpha = 1f;
     }
 }
