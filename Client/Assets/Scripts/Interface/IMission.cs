86ea4d2fc2d7d7a56088639ce1cc2c3e093966cf
Anglik <seonwoo6429@gmail.com>
2022-04-12 09:53:31 +0900

    이제와서 미션 틀 잡기

---
+++ Client/Assets/Scripts/Interface/IMission.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMission
+{
+    public CanvasGroup Cvs { get; }
+    public MissionType MissionType { get; }
+
+    public void Init();
+}
