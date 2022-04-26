6200a0783cc3d1cfcd10d8de0d1519d294bf0251
hwanjuson <hwanjuson@gmail.com>
2022-04-26 21:34:11 +0900

    sabotage so 수정

--- Client/Assets/Scripts/Data/SO/SabotageSO.cs
+++ Client/Assets/Scripts/Data/SO/SabotageSO.cs
@@ -8,9 +8,9 @@ using UnityEngine.Events;
 public class SabotageSO : ScriptableObject
 {
     public string sabotageName;
+    public string callbackName;
     public float coolTime;
+    public float shareCoolTime;
     public bool isShareCoolTime;
     public Sprite sabotageSprite;
-
-    public UnityEvent callback;
 }
