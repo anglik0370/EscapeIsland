366e00e56aa53e56187909476d1b47c3ee05082a
hwanjuson <hwanjuson@gmail.com>
2022-05-02 18:23:45 +0900

    게임이 종료되도 이름 색이 그대로인 버그 수정

--- Client/Assets/Scripts/Controll/Player/Player.cs
+++ Client/Assets/Scripts/Controll/Player/Player.cs
@@ -264,7 +264,7 @@ public class Player : MonoBehaviour, IInteractionObject
         }
 
         gameObject.SetActive(false);
-        ui.txtName.color = Color.black;
+        ui.SetNameTextColor(Color.black);
         ui.gameObject.SetActive(false);
     }
 
