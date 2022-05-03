a4e2fb5230ba4aeddb81f4788709a34b37121ad2
Anglik <seonwoo6429@gmail.com>
2022-05-03 18:48:37 +0900

    SpritePositionSort 수정

--- Client/Assets/Scripts/Utill/SpritePositionSort.cs
+++ Client/Assets/Scripts/Utill/SpritePositionSort.cs
@@ -6,6 +6,7 @@ public class SpritePositionSort : MonoBehaviour
 {
     [SerializeField]
     private bool bRunOnce = true;
+    private bool bInitOnce = false;
 
     [SerializeField]
     private Transform otherTrm;
@@ -14,33 +15,57 @@ public class SpritePositionSort : MonoBehaviour
     private bool useOtherTrm = false;
 
     [SerializeField]
-    private bool useReverse = false;
+    private List<SpriteRenderer> srList;
+    private List<int> originOrderList;
 
     [SerializeField]
-    private float posOffsetY;
+    private bool useSrList = false;
 
     private SpriteRenderer spriteRenderer;
 
-    private void Awake()
-    {
-        spriteRenderer = this.GetComponent<SpriteRenderer>();
-
-        posOffsetY = -transform.localPosition.y;
-    }
-
     private void LateUpdate()
     {
-        float precisionMultiplier = 10f;
-        //spriteRenderer.sortingOrder = (int)(-(transform.position.y + posOffsetY) * precisionMultiplier);
-
-        if (useReverse) precisionMultiplier *= -1;
-
-        if(useOtherTrm)
+        if(!bInitOnce)
         {
-            spriteRenderer.sortingOrder = (int)((0 - otherTrm.position.y) * precisionMultiplier);
+            if (useSrList)
+            {
+                originOrderList = new List<int>();
+
+                for (int i = 0; i < srList.Count; i++)
+                {
+                    originOrderList.Add(srList[i].sortingOrder);
+                }
+            }
+            else
+            {
+                spriteRenderer = this.GetComponent<SpriteRenderer>();
+            }
+            bInitOnce = true;
+        }
+
+        float precisionMultiplier = 100f;
+
+        if (useOtherTrm)
+        {
+            if (useSrList)
+            {
+                for (int i = 0; i < srList.Count; i++)
+                {
+                    srList[i].sortingOrder = (int)((0 - otherTrm.position.y) * precisionMultiplier) + originOrderList[i];
+                }
+            }
+            else
+            {
+                spriteRenderer.sortingOrder = (int)((0 - otherTrm.position.y) * precisionMultiplier);
+            }
         }
         else
         {
+            if (useSrList)
+            {
+                Debug.LogWarning("이거 쓸라면 useOtherTrm도 키라고 헀잖아");
+            }
+
             spriteRenderer.sortingOrder = (int)((0 - transform.position.y) * precisionMultiplier);
         }
 
