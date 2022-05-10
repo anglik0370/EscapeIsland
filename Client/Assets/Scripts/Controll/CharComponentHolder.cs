c24ccef6fca682a94c68bbfe116148a28c243dad
Anglik <seonwoo6429@gmail.com>
2022-05-10 12:55:20 +0900

    누락된 스크립트 추가

---
+++ Client/Assets/Scripts/Controll/CharComponentHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharComponentHolder : MonoBehaviour
+{
+    public Animator anim;
+    public SpriteRenderer[] sprites;
+}
