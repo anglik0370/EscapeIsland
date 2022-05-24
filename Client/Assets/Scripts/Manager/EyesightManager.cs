5831b936af66e2868ff44792bd502676e815f5f6
Anglik <seonwoo6429@gmail.com>
2022-05-24 15:38:32 +0900

    버그수정 + 구역에 따라서 보이는 오브젝트 수정

--- Client/Assets/Scripts/Manager/EyesightManager.cs
+++ Client/Assets/Scripts/Manager/EyesightManager.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class EyesightManager : MonoBehaviour
 {
     private Player player;
-    private Collider2D playerCol;
 
     [SerializeField]
-    private Collider2D labAreaCollider;
+    private Transform objectParentTrm;
+
     [SerializeField]
-    private Transform labObjParentTrm;
-    [SerializeField]
-    private List<Transform> labObjList;
-    [SerializeField]
-    private List<GameObject> anotherLabObjList;
+    private List<GameObject> otherList;
+
+    private List<AreaStateHolder> areaStateHolderList;
 
     private void Awake()
     {
-        labObjList = labObjParentTrm.GetComponentsInChildren<Transform>().ToList();
+        areaStateHolderList = objectParentTrm.GetComponentsInChildren<AreaStateHolder>().ToList();
     }
 
     void Start()
@@ -28,27 +27,19 @@ public class EyesightManager : MonoBehaviour
         {
             player = p;
         });
-
-        EventManager.SubGameOver(goc =>
-        {
-            LabObjSetActive(false);
-        });
     }
 
     void Update()
     {
         if (player == null) return;
 
-        LabObjSetActive(Physics2D.IsTouching(labAreaCollider, player.FootCollider));
+        for (int i = 0; i < areaStateHolderList.Count; i++)
+        {
+            areaStateHolderList[i].Sr.color = areaStateHolderList[i].AreaState == player.AreaState ? UtilClass.opacityColor : UtilClass.limpidityColor;
+        }
+
+        otherList[0].SetActive(player.AreaState == AreaState.BottleStorage);
+        otherList[1].SetActive(player.AreaState == AreaState.RefineryInLab);
     }
-
-    private void LabObjSetActive(bool active)
-    {
-        if (labObjList[0].gameObject.activeSelf == active) return;
-
-        labObjList.ForEach(x => x.gameObject.SetActive(active));
-        anotherLabObjList.ForEach(x => x.SetActive(active));
-    }
-
     //이녀석은 각 방 안에 플레이어가 있는지를 검사하고 그에 따라 오브젝트를 껏다켰다 해주는 놈이다
 }
