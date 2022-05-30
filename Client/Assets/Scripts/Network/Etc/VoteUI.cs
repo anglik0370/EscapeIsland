bedf656ebe7c90497bd20b121def45f5fb06a928
hwanjuson <hwanjuson@gmail.com>
2022-05-30 21:21:00 +0900

    그냥 유저 버튼 클릭시 투표 완료되게 (아직 터치는 안막아둠)

--- Client/Assets/Scripts/Network/Etc/VoteUI.cs
+++ Client/Assets/Scripts/Network/Etc/VoteUI.cs
@@ -7,43 +7,78 @@ public class VoteUI : MonoBehaviour
 {
     public int socId;
 
+    public Button sendVoteBtn;
+
     public Text nickNameText;
     public Image charImg;
-    public Toggle checkToggle;
     public Image voteCompleteImg;
+    public Image deadImg;
 
-    public void SetVoteUI(int socId, string name, Sprite charSprite, ToggleGroup group,bool isKidnapper)
+    public Transform userCountParent;
+
+    private void Start()
+    {
+        sendVoteBtn.onClick.AddListener(SendComplete);
+    }
+
+    public void SendComplete()
+    {
+        VoteCompleteVO vo = new VoteCompleteVO(NetworkManager.instance.socketId, socId);
+        DataVO dataVO = new DataVO("VOTE_COMPLETE", JsonUtility.ToJson(vo));
+        SocketClient.SendDataToSocket(JsonUtility.ToJson(dataVO));
+    }
+
+    public void SetVoteUI(int socId, string name, Sprite charSprite, bool isKidnapper)
     {
         this.socId = socId;
         nickNameText.text = name;
         nickNameText.color = isKidnapper && PlayerManager.Instance.AmIKidnapper() ? Color.red : Color.black;
         charImg.sprite = charSprite;
-        checkToggle.group = group;
 
-        checkToggle.isOn = false;
-        if(NetworkManager.instance.User.isDie || NetworkManager.instance.GetPlayerDie(socId))
+        if (NetworkManager.instance.GetPlayerDie(socId))
         {
-            ToggleOnOff(false);
-
-        }
-        else
-        {
-            ToggleOnOff(true);
+            OnDead();
         }
 
+        InitTargeted();
         voteCompleteImg.gameObject.SetActive(false);
 
         OnOff(true);
     }
 
+    public void OnDead()
+    {
+        DeadImgActive(true);
+    }
+
+    private void DeadImgActive(bool active)
+    {
+        deadImg.gameObject.SetActive(active);
+    }
+
+    public void VoteTargeted()
+    {
+        UserImg userImg = PoolManager.GetItem<UserImg>();
+
+        userImg.transform.SetParent(userCountParent);
+    }
+
+    public void InitTargeted()
+    {
+        for (int i = 0; i < userCountParent.childCount; i++)
+        {
+            GameObject userImg = userCountParent.GetChild(i).gameObject;
+
+            if(userImg.activeSelf)
+            {
+                userImg.SetActive(false);
+            }
+        }
+    }
+
     public void OnOff(bool on)
     {
         gameObject.SetActive(on);
-    }
-
-    public void ToggleOnOff(bool on)
-    {
-        checkToggle.gameObject.SetActive(on);
     }
 
     public void VoteComplete()
