3c7703e95c9b83c6af0a5a29fdef9716bbe8f70c
hwanjuson <hwanjuson@gmail.com>
2022-06-21 10:21:24 +0900

    오류 수정

--- _Prototype/Client/Assets/Scripts/Editor/StorageEditor.cs
+++ _Prototype/Client/Assets/Scripts/Editor/StorageEditor.cs
@@ -7,7 +7,7 @@ public class StorageEditor : EditorWindow
 {
     private bool isGameStarted;
 
-    private ItemSO selectedItem;
+    //private ItemSO selectedItem;
     private int amount;
 
     private bool fillAllOnce;
@@ -19,96 +19,96 @@ public class StorageEditor : EditorWindow
         window.Show();
     }
 
-    private void OnGUI()
-    {
-        GUILayout.BeginVertical();
+    //private void OnGUI()
+    //{
+    //    GUILayout.BeginVertical();
 
-        GUILayout.Space(20.0f);
+    //    GUILayout.Space(20.0f);
 
-        GUILayout.Label("Storage Editor", EditorStyles.boldLabel);
+    //    GUILayout.Label("Storage Editor", EditorStyles.boldLabel);
 
-        GUILayout.Space(10.0f);
+    //    GUILayout.Space(10.0f);
 
-        if(isGameStarted)
-        {
-            GUILayout.BeginHorizontal();
+    //    if(isGameStarted)
+    //    {
+    //        GUILayout.BeginHorizontal();
 
-            GUILayout.Label("Selected Item");
+    //        GUILayout.Label("Selected Item");
 
-            selectedItem = EditorGUILayout.ObjectField(selectedItem, typeof(ItemSO), false) as ItemSO;
+    //        selectedItem = EditorGUILayout.ObjectField(selectedItem, typeof(ItemSO), false) as ItemSO;
 
-            amount = EditorGUILayout.IntField(amount, GUILayout.Width(40f));
+    //        amount = EditorGUILayout.IntField(amount, GUILayout.Width(40f));
 
-            GUILayout.EndHorizontal();
+    //        GUILayout.EndHorizontal();
 
-            GUILayout.Space(10.0f);
+    //        GUILayout.Space(10.0f);
 
-            GUILayout.BeginHorizontal();
+    //        GUILayout.BeginHorizontal();
 
-            if (GUILayout.Button("Add"))
-            {
-                if (selectedItem != null)
-                {
-                    for (int i = 0; i < amount; i++)
-                    {
-                        StorageManager.Instance.AddItem(selectedItem);
-                    }
-                }
-            }
+    //        if (GUILayout.Button("Add"))
+    //        {
+    //            if (selectedItem != null)
+    //            {
+    //                for (int i = 0; i < amount; i++)
+    //                {
+    //                    StorageManager.Instance.AddItem(selectedItem);
+    //                }
+    //            }
+    //        }
 
-            if (GUILayout.Button("Remove"))
-            {
-                if (selectedItem != null)
-                {
-                    for (int i = 0; i < amount; i++)
-                    {
-                        StorageManager.Instance.RemoveItem(selectedItem);
-                    }
-                }
-            }
+    //        if (GUILayout.Button("Remove"))
+    //        {
+    //            if (selectedItem != null)
+    //            {
+    //                for (int i = 0; i < amount; i++)
+    //                {
+    //                    StorageManager.Instance.RemoveItem(selectedItem);
+    //                }
+    //            }
+    //        }
 
-            GUILayout.EndHorizontal();
+    //        GUILayout.EndHorizontal();
 
-            EditorGUILayout.HelpBox("아이템을 한번에 모두 채우려면 아래의 버튼을 누르세요", MessageType.Info);
+    //        EditorGUILayout.HelpBox("아이템을 한번에 모두 채우려면 아래의 버튼을 누르세요", MessageType.Info);
 
-            if(GUILayout.Button("Fill All Item"))
-            {
-                if (!fillAllOnce)
-                {
-                    StorageManager.Instance.FillAllItem();
+    //        if(GUILayout.Button("Fill All Item"))
+    //        {
+    //            if (!fillAllOnce)
+    //            {
+    //                StorageManager.Instance.FillAllItem();
 
-                    fillAllOnce = true;
-                }
-            }
-        }
-        else
-        {
-            EditorGUILayout.HelpBox("게임이 사직되지 않았습니다 " + isGameStarted, MessageType.Error);
-        }
+    //                fillAllOnce = true;
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        EditorGUILayout.HelpBox("게임이 사직되지 않았습니다 " + isGameStarted, MessageType.Error);
+    //    }
 
-        GUILayout.EndVertical();
-    }
+    //    GUILayout.EndVertical();
+    //}
 
-    private void Awake()
-    {
-        isGameStarted = false;
+    //private void Awake()
+    //{
+    //    isGameStarted = false;
 
-        EventManager.SubGameStart(p =>
-        {
-            isGameStarted = true;
-            fillAllOnce = false;
-        });
+    //    EventManager.SubGameStart(p =>
+    //    {
+    //        isGameStarted = true;
+    //        fillAllOnce = false;
+    //    });
 
-        EventManager.SubGameOver(goc =>
-        {
-            isGameStarted = false;
-            fillAllOnce = false;
-        });
+    //    EventManager.SubGameOver(goc =>
+    //    {
+    //        isGameStarted = false;
+    //        fillAllOnce = false;
+    //    });
 
-        EventManager.SubExitRoom(() =>
-        {
-            isGameStarted = false;
-            fillAllOnce = false;
-        });
-    }
+    //    EventManager.SubExitRoom(() =>
+    //    {
+    //        isGameStarted = false;
+    //        fillAllOnce = false;
+    //    });
+    //}
 }
