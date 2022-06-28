adba2156ddb1b6829440b01e56af9b9d2db6cca7
hwanjuson <hwanjuson@gmail.com>
2022-06-28 15:31:22 +0900

    체리 스킬 완성

--- _Prototype/Client/Assets/Scripts/Data/SO/BuffSO/CoolTimeBuffSO.cs
+++ _Prototype/Client/Assets/Scripts/Data/SO/BuffSO/CoolTimeBuffSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "new BuffSO", menuName = "SO/BuffSO/CoolTimeBuffSO")]
 public class CoolTimeBuffSO : BuffSO
 {
     public float magnification;
