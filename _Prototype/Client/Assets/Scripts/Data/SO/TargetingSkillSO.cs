19d7f921616ac0861795f9da609143090477e1c0
hwanjuson <hwanjuson@gmail.com>
2022-06-29 18:52:20 +0900

    ray skill 관련 스크립트 작성

---
+++ _Prototype/Client/Assets/Scripts/Data/SO/TargetingSkillSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/TargetingSkillSO", fileName = "New SkillSO")]
+public class TargetingSkillSO : SkillSO
+{
+    public float skillRange;
+}
