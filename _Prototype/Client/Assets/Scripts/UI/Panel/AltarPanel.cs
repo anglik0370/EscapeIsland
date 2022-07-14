3a9f6c586131a95af896e376d8c5d7e79cae8e37
anglik0370 <seonwoo6429@gmail.com>
2022-07-14 15:57:30 +0900

    제단 패널 개발중

--- _Prototype/Client/Assets/Scripts/UI/Panel/AltarPanel.cs
+++ _Prototype/Client/Assets/Scripts/UI/Panel/AltarPanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AltarPanel : Panel
@@ -10,12 +11,33 @@ public class AltarPanel : Panel
     private int itemCnt = 0;
 
     [SerializeField]
+    private Transform slotParentTrm;
     private List<AltarSlot> slots = new List<AltarSlot>();
 
     protected override void Awake()
     {
         base.Awake();
 
+        slots = slotParentTrm.GetComponentsInChildren<AltarSlot>().ToList();
+
         maxItemCnt = slots.Count;
+    }
+
+    public void DropItem()
+    {
+        if(itemCnt < maxItemCnt)
+        {
+            itemCnt++;
+        }
+        else
+        {
+            itemCnt = 0;
+
+            //슬롯 초기화 해주고
+            slots.ForEach(x => x.SetItem(null));
+
+            //여기서 버프를 주면 됨
+
+        }
     }
 }
