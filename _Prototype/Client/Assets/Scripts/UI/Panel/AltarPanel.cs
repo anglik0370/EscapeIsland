2cea706e4e9e3e95efe9a718ed677f434e428af8
hwanjuson <hwanjuson@gmail.com>
2022-08-08 15:02:47 +0900

    제단 ui 대강 수정 및 코드 작성중

--- _Prototype/Client/Assets/Scripts/UI/Panel/AltarPanel.cs
+++ _Prototype/Client/Assets/Scripts/UI/Panel/AltarPanel.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AltarPanel : Panel
 {
@@ -14,6 +15,13 @@ public class AltarPanel : Panel
     private Transform slotParentTrm;
     private List<AltarSlot> slots = new List<AltarSlot>();
 
+    [SerializeField]
+    private Button offerBtn;
+
+    [SerializeField]
+    private int defaultProbability = 30;
+    private int currentProbability = 30;
+
     protected override void Awake()
     {
         base.Awake();
@@ -21,6 +29,48 @@ public class AltarPanel : Panel
         slots = slotParentTrm.GetComponentsInChildren<AltarSlot>().ToList();
 
         maxItemCnt = slots.Count;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        offerBtn.onClick.AddListener(OnClickOfferBtn);
+    }
+
+    private void OnClickOfferBtn()
+    {
+        if(AltarSlotsIsNull())
+        {
+            UIManager.Instance.AlertText("최소 한개 이상의 재료가 필요합니다.", AlertType.Warning);
+            return;
+        }
+
+        int idx = Random.Range(1, 101);
+
+        if(idx <= currentProbability)
+        {
+            //버프 돌리기
+        }
+        else
+        {
+
+        }
+    }
+
+    private bool AltarSlotsIsNull()
+    {
+        int count = 0;
+
+        for (int i = 0; i < slots.Count; i++)
+        {
+            if(!slots[i].IsEmpty)
+            {
+                count++;
+            }
+        }
+
+        return count <= 0;
     }
 
     public void DropItem()
