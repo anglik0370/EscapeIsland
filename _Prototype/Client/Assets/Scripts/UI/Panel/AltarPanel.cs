2b7793e3a01ad27844ad20a53f793a248d8385cd
hwanjuson <hwanjuson@gmail.com>
2022-08-08 16:34:52 +0900

    제단 버프 구분용 코드 작성 / 제단 클라이언트 코드 작성

--- _Prototype/Client/Assets/Scripts/UI/Panel/AltarPanel.cs
+++ _Prototype/Client/Assets/Scripts/UI/Panel/AltarPanel.cs
@@ -19,14 +19,28 @@ public class AltarPanel : Panel
     private Button offerBtn;
 
     [SerializeField]
+    private Text effectText;
+
+    [SerializeField]
     private int defaultProbability = 30;
     private int currentProbability = 30;
+
+    [SerializeField]
+    private int lastBuffIsNerfProbability = 5;
+    private List<int> probabilityList;
+
+    private BuffSO lastBuff = null;
 
     protected override void Awake()
     {
         base.Awake();
 
         slots = slotParentTrm.GetComponentsInChildren<AltarSlot>().ToList();
+
+        probabilityList = new List<int>()
+        {
+            0,15,20,25
+        };
 
         maxItemCnt = slots.Count;
     }
@@ -40,37 +54,42 @@ public class AltarPanel : Panel
 
     private void OnClickOfferBtn()
     {
-        if(AltarSlotsIsNull())
+        currentProbability = defaultProbability;
+
+        for (int i = 0; i < slots.Count; i++)
+        {
+            AltarSlot slot = slots[i];
+
+            if(slot.IsEmpty) continue;
+
+            currentProbability += probabilityList[slot.GetItem().tier];
+        }
+
+        if (lastBuff != null && !lastBuff.isBuffed)
+        {
+            currentProbability += lastBuffIsNerfProbability;
+        }
+
+        if(currentProbability <= defaultProbability)
         {
             UIManager.Instance.AlertText("최소 한개 이상의 재료가 필요합니다.", AlertType.Warning);
             return;
         }
 
         int idx = Random.Range(1, 101);
+        int buffId = -1;
 
-        if(idx <= currentProbability)
+        if (idx <= currentProbability)
         {
             //버프 돌리기
+            buffId = BuffManager.Instance.GetAltarBuffId();
         }
         else
         {
-
-        }
-    }
-
-    private bool AltarSlotsIsNull()
-    {
-        int count = 0;
-
-        for (int i = 0; i < slots.Count; i++)
-        {
-            if(!slots[i].IsEmpty)
-            {
-                count++;
-            }
+            effectText.text = "꽝";
         }
 
-        return count <= 0;
+        SendManager.Instance.SendAltar(new AltarVO(NetworkManager.instance.socketId, buffId));
     }
 
     public void DropItem()
