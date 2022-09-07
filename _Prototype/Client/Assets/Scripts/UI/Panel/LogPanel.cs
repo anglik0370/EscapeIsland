0af406653811abe819e1169bc806adb310e37022
anglik0370 <seonwoo6429@gmail.com>
2022-09-07 11:03:29 +0900

    점령, 모음 로그

--- _Prototype/Client/Assets/Scripts/UI/Panel/LogPanel.cs
+++ _Prototype/Client/Assets/Scripts/UI/Panel/LogPanel.cs
@@ -20,7 +20,7 @@ public class LogPanel : MonoBehaviour
 
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
         }
@@ -44,16 +44,55 @@ public class LogPanel : MonoBehaviour
 
     public void GlobalSkillLog(Player caster, string skillName)
     {
-        string str = $"[<color={caster.CurTeam.ToString().ToLower()}>{caster.socketName}</color>] {skillName} 사용";
+        string str = $"<color={caster.CurTeam.ToString().ToLower()}>{caster.socketName}</color>이 {skillName}을(를) 사용하였습니다";
         CreateLogText(str);
     }
 
     public void SingleSkillLog(Player caster, Player victim, string skillName)
     {
-        string str = $"[<color={caster.CurTeam.ToString().ToLower()}>{caster.socketName}</color>] <color={victim.CurTeam.ToString().ToLower()}>{victim.socketName}</color> 에게 {skillName} 사용";
+        string str = $"<color={caster.CurTeam.ToString().ToLower()}>{caster.socketName}</color>이 <color={victim.CurTeam.ToString().ToLower()}>{victim.socketName}</color> 에게 {skillName}을(를) 사용하였습니다";
         CreateLogText(str);
     }
 
+    public void OccupationLog(Team team, Area area)
+    {
+        string teamStr = team == Team.BLUE ? "블루팀" : "레드팀";
+
+        string areaStr = "";
+
+        switch (area)
+        {
+            case Area.Beach:
+                areaStr = "해변";
+                break;
+            case Area.Cave:
+                areaStr = "동굴";
+                break;
+            case Area.Field:
+                areaStr = "밭";
+                break;
+            case Area.Forest:
+                areaStr = "숲";
+                break;
+        }
+
+        if(areaStr.Equals("")){
+            Debug.Log("잘못된 구역 로그");
+            return;
+        }
+
+        string str = $"<color={team.ToString().ToLower()}>{teamStr}</color>이 {areaStr}을(를) 점령하였습니다";
+        CreateLogText(str);
+    }
+
+    public void StorageFullLog(Team team, ItemSO item)
+    {
+        string teamStr = team == Team.BLUE ? "블루팀" : "레드팀";
+
+        string str = $"<color={team.ToString().ToLower()}>{teamStr}</color>이 {item.itemName}을(를) 모두 모았습니다";
+        CreateLogText(str);
+    }
+    
     public void ClearLog()
     {
         foreach (var log in logTexts)
